aa6e58514fb68692efa2f3df37ddb9af1e287106
yapkm01 <yapkm01@hotmail.com>
2025-02-16 11:54:22 -0500

    continuous update revision chapter1 & 16 - 1

--- chapter16/16.3.cc
+++ chapter16/16.3.cc
@@ -12,7 +12,7 @@
 using namespace std;
 
 template <typename T>
-int compare (const T &v1, const T &v2) {
+int compare (const T& v1, const T& v2) {
 	if (less<T>()(v1, v2)) return -1;
 	if (less<T>()(v2, v1)) return 1;
 	return 0;

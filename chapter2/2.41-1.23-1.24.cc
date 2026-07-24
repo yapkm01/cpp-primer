56a92433b4d47ac2d74c9ee04f3ebabd211c5154
yapkm01 <yapkm01@hotmail.com>
2026-07-24 19:56:14 -0400

    update 2.41-1.23-1.24.cc

---
+++ chapter2/2.41-1.23-1.24.cc
@@ -0,0 +1,48 @@
+/*
+ * Exercise 2.41:
+ * Use your Sales_data class to rewrite the exercises in § 1.5.1
+ * (p. 22), § 1.5.2 (p. 24), and § 1.6 (p. 25). For now, you should define your
+ * Sales_data class in the same file as your main function.
+ *
+ * Exercise 1.23:
+ * Write a program that reads several transactions and counts
+ * how many transactions occur for each ISBN.
+ *
+ * Exercise 1.24:
+ * Test the previous program by giving transactions representing multiple
+ * ISBNS. The records for each ISBN should be grouped together
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Sales_data {
+	string book_no;
+	unsigned units_sold = 0;
+	double revenue = 0.;
+};
+
+int main() {
+	Sales_data curr_item, prev_item;
+	int count = 0;
+	if (cin >> prev_item.book_no) {
+		double price = 0.;
+		cin >> prev_item.units_sold >> price;
+		count += 1;
+		while (cin >> curr_item.book_no >> curr_item.units_sold >> price) 
+		   if (curr_item.book_no == prev_item.book_no) 
+			count += 1;
+		   else {
+			cout << "# txns for ISBN " << prev_item.book_no << " = " << count << endl;
+			prev_item.book_no = curr_item.book_no;
+			prev_item.units_sold = curr_item.units_sold;
+			count = 1;
+		   } 
+		cout << "# txns for ISBN " << curr_item.book_no << " = " << count << endl;
+	} else {
+		cerr << "No data?!" << endl;
+		return -1;
+	}
+	return 0;
+}
